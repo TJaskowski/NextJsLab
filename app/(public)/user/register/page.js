7a8fb581846f9a288c7e17277b276f7412342e4b
Tomasz J <tomasz.jaskowski@microsoft.wsei.edu.pl>
2025-01-05 21:31:20 +0100

    Validation of RegisterForm and onSubmit added

--- app/(public)/user/register/page.js
+++ app/(public)/user/register/page.js
@@ -1,19 +1,87 @@
-export default function Register() {
+'use client';
+import { useRouter } from 'next/navigation';
+
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, signOut } from "firebase/auth";
+import { useAuth } from "@/app/lib/AuthContext";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export default function RegisterForm(){
+
+
+  const { user } = useAuth();
+  const router = useRouter();
+  
+  if (user) {
+    return null;
+  }
+  
+  const auth = getAuth();
+  
+  const [registerError, setRegisterError] = useState(""); //stan błędów rejestracji
+    const { register, handleSubmit} = useForm();
+  
+  
+  const onSubmit = (data) => {
+    console.log("clicked");
+    // walidacja obu równości haseł  
+    if (data.password !== data.confirmPassword) {
+        setRegisterError("Passwords do not match!");
+        return;
+      }
+    createUserWithEmailAndPassword(auth, data.email, data.password)
+      .then((userCredential) => {
+        console.log("User registered!");
+        sendEmailVerification(auth.currentUser)
+          .then(() => {
+            console.log("Email verification send!");
+            router.push("/user/verify");
+        });
+        
+      })
+      .catch((error) => {
+        setRegisterError(error.message);
+        console.dir(error);
+      });
+  };
   return (
     <div className="card items-center shadow-lg p-6 w-100">
       <h1>Rejestracja</h1>
-      <form className="form-control">
+      {registerError && (
+          <div className="alert alert-error">
+            <p>{registerError}</p>
+          </div>)}
+      <form className="form-control" onSubmit={handleSubmit(onSubmit)}>
+        <div>
       <label className="input input-bordered flex items-center gap-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
           fill="currentColor"
-          className="h-4 w-4 opacity-70"
-        >
+          className="h-4 w-4 opacity-70">
           <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
           <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-        </svg>
-        <input type="text" className="grow" placeholder="Email" />
+          </svg>
+        <input  id="email"
+              name="email"
+              type="email"
+              className="grow"
+              placeholder="Email"
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Musisz podać adres email!",
+                },
+                maxLength: {
+                  value: 40,
+                  message: " Adres email jest zbyt długi!",
+                },
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Niepoprawny format e-mail",
+                },
+              })}/>
+             
       </label>
       <label className="input input-bordered flex items-center gap-2">
       <svg
@@ -28,7 +96,18 @@ export default function Register() {
             clipRule="evenodd"
           />
         </svg>
-        <input type="text" className="grow" placeholder="Password" />
+        <input id="password"
+              name="password"
+              type="password"
+              className="grow"
+              placeholder="Your password"
+              {...register("password", {
+                required: "Hasło jest wymagane!",
+                maxLength: {
+                  value: 20,
+                  message: "Hasło jest za długie!",
+                },
+              })} />
       </label>
       <label className="input input-bordered flex items-center gap-2">
         <svg
@@ -43,9 +122,18 @@ export default function Register() {
             clipRule="evenodd"
           />
         </svg>
-        <input type="text" className="grow" placeholder="Password" />
+        <input name="confirmPassword" type="password" className="grow" placeholder="Confirm Password"
+        {...register("confirmPassword", {
+            required: "Hasło jest wymagane!",
+            maxLength: {
+              value: 20,
+              message: "Hasło jest za długie!",
+            },
+          })} />
       </label>
-      <label type="submit"  className="btn btn-primary " >Zapisz</label>
+      </div>
+
+      <button type="submit"  className="btn btn-primary " >Zapisz</button>
       </form>
 
     </div>
